089e15d618125820e816db2873f80f5351a8ee94
cpena-minery <carlos.cpa26@gmail.com>
2024-10-17 08:03:56 -0600

    arreglando y agregando logica CRUD a entidad noticia con vista y estilo Co-authored-by: penaherrera <carlos.cpa26@gmail.com>

--- src/main/java/sv/edu/udb/dwfcatedra/beans/NoticiaBean.java
+++ src/main/java/sv/edu/udb/dwfcatedra/beans/NoticiaBean.java
@@ -1,25 +1,39 @@
 package sv.edu.udb.dwfcatedra.beans;
 
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
 import lombok.Getter;
 import lombok.Setter;
-import sv.edu.udb.dwfcatedra.repository.NoticiaRepository;
 import jakarta.annotation.PostConstruct;
-import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import sv.edu.udb.dwfcatedra.repository.domain.Noticia;
+import sv.edu.udb.dwfcatedra.service.NoticiaService;
 
 @Named
-@RequestScoped
+@ViewScoped
 public class NoticiaBean implements Serializable {
 
     @Inject
-    private NoticiaRepository noticiaRepository;
+    private NoticiaService noticiaService;
 
     @Getter @Setter
     private Long id;
+
+    @Getter @Setter
+    private String titulo;
+
+    @Getter @Setter
+    private String descripcion;
+
+    @Getter @Setter
+    private String fecha;
 
     @Getter @Setter
     private Noticia noticia;
@@ -27,20 +41,101 @@ public class NoticiaBean implements Serializable {
     @Getter
     private List<Noticia> noticias;
 
+    @Getter @Setter
+    private int currentPage = 0;
+
+    @Getter @Setter
+    private int rowsPerPage = 4;
+
+    @Getter
+    private int totalRows;
+
     @PostConstruct
     public void init() {
+        if (this.id != null) {
+            loadNoticia();
+        }
+        else{
+            noticia = new Noticia();
+        }
         loadNoticias();
     }
 
-    // Cargar una noticia por id
     public void loadNoticia() {
-        if (id != null) {
-            noticia = noticiaRepository.findById(id);
+            noticia = noticiaService.getNoticiaById(this.id);
+    }
+
+    public void loadNoticias() {
+        totalRows = (int) noticiaService.getTotalNoticiasCount();
+        paginateNoticias();
+    }
+
+    public void paginateNoticias() {
+        if (totalRows > 0) {
+            int start = currentPage * rowsPerPage;
+            int end = Math.min(start + rowsPerPage, totalRows);
+            noticias = noticiaService.getAllNoticias().subList(start, end);
+        } else {
+            noticias = new ArrayList<>();
+        }
+    }
+    public void nextPage() {
+        if ((currentPage + 1) * rowsPerPage < totalRows) {
+            currentPage++;
+            paginateNoticias();
         }
     }
 
-    // Cargar todas las noticias
-    public void loadNoticias() {
-        noticias = noticiaRepository.findAll();
+    public void prevPage() {
+        if (currentPage > 0) {
+            currentPage--;
+            paginateNoticias();
+        }
     }
+
+
+    public String redirectUpdateNoticia(Long noticiaId) {
+        this.id = noticiaId;  // Asigna el id de la noticia a editar
+        return "editarNoticia.xhtml?faces-redirect=true&amp;id=" + this.id;
+    }
+
+    public String eliminarNoticia(Long noticiaId) {
+        noticiaService.deleteNoticiaById(noticiaId);
+        return "noticiasAdmin.xhtml?faces-redirect=true";
+    }
+
+    public String createNoticia() {
+        Noticia nuevaNoticia = new Noticia();
+        nuevaNoticia.setTitulo(this.noticia.getTitulo());
+        nuevaNoticia.setDescripcion(this.noticia.getDescripcion());
+        nuevaNoticia.setFecha(this.noticia.getFecha());
+
+        noticiaService.saveNoticia(nuevaNoticia);
+
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Noticia creada con éxito", null));
+
+        return "noticiasAdmin.xhtml?faces-redirect=true";
+    }
+
+    public String update() {
+        Noticia noticiaEditada = new Noticia();
+        noticiaEditada.setId(this.id);
+        noticiaEditada.setTitulo(this.noticia.getTitulo());
+        noticiaEditada.setDescripcion(this.noticia.getDescripcion());
+        noticiaEditada.setFecha(this.noticia.getFecha());
+
+        noticiaService.updateNoticia(noticiaEditada);
+
+        return "noticiasAdmin.xhtml?faces-redirect=true";
+    }
+
+
+    public String formatFecha(Date fecha) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        return sdf.format(fecha);
+    }
+
 }
+
+
+
--- src/main/java/sv/edu/udb/dwfcatedra/repository/NoticiaRepository.java
+++ src/main/java/sv/edu/udb/dwfcatedra/repository/NoticiaRepository.java
@@ -2,6 +2,8 @@ package sv.edu.udb.dwfcatedra.repository;
 import jakarta.inject.Named;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import sv.edu.udb.dwfcatedra.repository.domain.Noticia;
 import java.util.List;
 
@@ -18,8 +20,34 @@ public class NoticiaRepository {
                 .getResultList();
     }
 
+    public long count() {
+        Query query = entityManager.createQuery("SELECT COUNT(n) FROM Noticia n");
+        return (Long) query.getSingleResult();
+    }
+
     public Noticia findById(final Long id) {
         return entityManager.find(Noticia.class, id);
     }
+
+    @Transactional
+    public void save(final Noticia noticia) {
+        entityManager.persist(noticia);
+    }
+
+    @Transactional
+    public void update(final Noticia noticia) {
+        entityManager.merge(noticia);
+    }
+
+    @Transactional
+    public void delete(Noticia noticia) {
+        if (entityManager.contains(noticia)) {
+            entityManager.remove(noticia);
+        } else {
+            Noticia mergedNoticia = entityManager.merge(noticia);
+            entityManager.remove(mergedNoticia);
+        }
+    }
+
 
 }
